9f792e7c2ad6991ee7af9a39a09d863d83ca4677
lizZ-Ka <zzil2.0@mail.ru>
2024-05-17 23:27:37 +0300

    upd: create Header

--- app/src/widgets/Header/Header.jsx
+++ app/src/widgets/Header/Header.jsx
@@ -1,5 +1,33 @@
 import React from "react";
+import '../Header/Header.css'
+import logo from '../../assets/svg/logo.svg'
+import echpochmack from '../../assets/svg/echpochmack.svg'
+import profile_pic from '../../assets/png/profile_pic.png'
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+  <div className="header_wrapper">
+    <div className="logo_wrapper">
+      <img src={logo} className="logo" />
+    </div>
+    <div className="nav_wrapper">
+      <li className="list_item">
+          Друзья
+      </li>
+      <li className="list_item">
+          Форум
+      </li>
+      <li className="list_item">
+          Все курсы
+      </li>
+    </div>
+    <div className="echpochmack_counter">
+      <p>16</p>
+      <img src={echpochmack} />
+    </div>
+    <div className="profile_img">
+      <img src={profile_pic} />
+    </div>
+  </div>
+  );
 }
