45eb8365a711721541c6434faf3921b6e8d25e80
serdeshnow <vserdes1977@mail.ru>
2024-05-17 20:51:10 +0300

    upd: Button

--- app/src/pages/HomePage.jsx
+++ app/src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import "../styles/HomePage.css";
+import Button from "../widgets/Button/Button.jsx";
 
 export const HomePage = () => {
   return (
     <div className="page_wrapper">
       <h1>HomePage</h1>
       <p>Link to </p>
-      <button>button</button>
+      <Button>button</Button>
     </div>
   );
 };
--- app/src/widgets/Button/Button.jsx
+++ app/src/widgets/Button/Button.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import "./Button.css";
 
 export default function Button({ children, ...props }) {
-  return <button {...props}>{children}</button>;
+  return (
+    <button {...props} className="button">
+      {children}
+    </button>
+  );
 }
