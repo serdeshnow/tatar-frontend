4db8e52e371d1057b5afeaff93bbfbd1b2d39473
lizZ-Ka <zzil2.0@mail.ru>
2024-05-18 01:11:03 +0300

    profile bim bam

--- app/src/pages/HomePage.jsx
+++ app/src/pages/HomePage.jsx
@@ -1,31 +1,13 @@
-import ProgressBar from "../components/ProgressBar/ProgressBar.js";
+
 import "../styles/HomePage.css";
 import Button from "../widgets/Button/Button.jsx";
 
 export const HomePage = () => {
   return (
-    <div className="page_wrapper">
-      <div className="left_part">
-        <div className="achievements">
-          <p>я ничего не добился</p>
-        </div>
-        <div className="battle">
-          <p>го пиздиться</p>
-        </div>
-      </div>
-      <div className="avatar_wrapper">
-        <img src="" alt='ёбик' />
-      </div>
-      <div className="progress-container">
-        <ProgressBar width={10}/>
-      <ProgressBar width={20}/>
-      <ProgressBar width={60}/>
-      <ProgressBar width={90}/>
-      </div>
-      
+   
 
       
       <Button>button</Button>
-    </div>
+    
   );
 };
--- app/src/pages/RegistrationPage.jsx
+++ app/src/pages/RegistrationPage.jsx
@@ -3,7 +3,8 @@ import "../styles/RegistrationPage.css";
 export const RegistrationPage = () => {
   return (
     <div className="page_wrapper">
-      <h1>RegistrationPage</h1>
+      
+      
     </div>
   );
 };
