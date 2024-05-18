5f6cb7197c8608cb7c50ffc3ffcc8174c71500fe
lizZ-Ka <zzil2.0@mail.ru>
2024-05-18 22:37:17 +0300

    alga bombar

--- app/src/pages/RegistrationIntroductionPage/RegistrationIntroduction2Page.jsx
+++ app/src/pages/RegistrationIntroductionPage/RegistrationIntroduction2Page.jsx
@@ -5,12 +5,17 @@ import Button from "../../widgets/Button/Button.jsx";
 export const RegistrationIntroduction2Page = () => {
   console.log("Reg2");
   return (
+    <div style={{height: '100vh', display: 'flex', alignItems: "center", justifyContent: "center", backgroundColor: "#FCFADD"}}>
+<div className="reg2_page">
     <div className="reg2_page_wrapper">
-      <h1 className="reg2_title"><a className="alga">Алга</a> - вперёд!</h1>
+      <h1 className="reg2_title"><a className="alga_txt">Алга</a> - вперёд!</h1>
       <h1 className="reg2_title">И мы не останавливаемся</h1>
       <Link to="/registration/introduction_3">
-        <button>Алга</button>
+        <button className="alga_btn">Алга</button>
       </Link>
     </div>
+    </div>
+    </div>
+    
   );
 };
