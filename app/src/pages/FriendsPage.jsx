c088082b7640a3b0f0be1a7429dd8c6ff1b8735d
serdeshnow <vserdes1977@mail.ru>
2024-05-18 14:17:14 +0300

    upd: TextForm added

--- app/src/pages/FriendsPage.jsx
+++ app/src/pages/FriendsPage.jsx
@@ -1,9 +1,16 @@
 import "../styles/FriendsPage.css";
+import TextFrom from "../widgets/TextForm/TextForm.jsx";
+import Button from "../widgets/Button/Button.jsx";
 
 export const FriendsPage = () => {
   return (
-    <div className="page_wrapper">
+    <div className="friends_wrapper page_wrapper">
       <h1>FriendsPage</h1>
+      <div className="form_wrapper">
+        <TextFrom labelTitle="Почта" id="mail" />
+        <TextFrom labelTitle="Пароль" id="password" />
+        <Button className="green_enter_btn">Войти</Button>
+      </div>
     </div>
   );
 };
