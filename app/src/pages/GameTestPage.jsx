fd0e75b3137664e1aed4aa32d2f3b2109ad6c98f
serdeshnow <vserdes1977@mail.ru>
2024-05-19 05:36:11 +0300

    upd: game

--- app/src/pages/GameTestPage.jsx
+++ app/src/pages/GameTestPage.jsx
@@ -1,23 +1,78 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles/GameTestPage.css";
 import RadioForm from "../widgets/RadioForm/RadioForm";
 
 export const GameTestPage = () => {
-  const [radioForm, setRadioForm] = useState(false);
-  const radioQuestion = "hui";
-  const id = "1";
+  const [button1, setButton1] = useState(false);
+  const radioQuestion = "bui";
+
+  const [curr, setCurr] = useState(1);
+  const [res, setRes] = useState({});
+
+  useEffect(() => {
+    console.log(Object.values(res));
+  }, [res]);
+
+  const getForm = () => {
+    switch (true) {
+      case curr == 1: {
+        return (
+          <RadioForm
+            curr={curr}
+            setCurr={setCurr}
+            res={res}
+            setRes={setRes}
+            q1={radioQuestion}
+            setChecked={setButton1}
+          />
+        );
+      }
+      case curr == 2: {
+        return (
+          <RadioForm
+            curr={curr}
+            setCurr={setCurr}
+            res={res}
+            setRes={setRes}
+            q1={radioQuestion}
+            setChecked={setButton1}
+          />
+        );
+      }
+      case curr == 3: {
+        return (
+          <RadioForm
+            curr={curr}
+            setCurr={setCurr}
+            res={res}
+            setRes={setRes}
+            q1={radioQuestion}
+            setChecked={setButton1}
+          />
+        );
+      }
+      case curr == 4: {
+        return (
+          <RadioForm
+            curr={curr}
+            setCurr={setCurr}
+            res={res}
+            setRes={setRes}
+            q1={radioQuestion}
+            setChecked={setButton1}
+          />
+        );
+      }
+    }
+  };
 
   return (
     <div className="page_wrapper">
       <h1>Test Name</h1>
       <p>Вопрос 1</p>
       <p className="question">Question</p>
-      <div className="forms_wrapper">
-        <RadioForm id={id} radioQuestion={radioQuestion} />
-        <RadioForm id="2" radioQuestion="huh" />
-      </div>
-
-      <button className="start_btn">Продолжить</button>
+      <div className="forms_wrapper">{getForm()}</div>
+      {/* <button className="start_btn">Продолжить</button> */}
     </div>
   );
 };
--- app/src/widgets/RadioForm/RadioForm.jsx
+++ app/src/widgets/RadioForm/RadioForm.jsx
@@ -1,27 +1,119 @@
 import "./RadioForm.css";
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export default function RadioForm({ radioQuestion, id }) {
-  const [checked, setChecked] = useState(true);
+export default function RadioForm({
+  q1 = "lui",
+  id1 = 1,
+  q2 = "hit",
+  id2 = 2,
+  q3 = "nit",
+  id3 = 3,
+  idcorrect = 1,
+  setChecked,
+  curr,
+  setCurr,
+  res,
+  setRes,
+}) {
+  const handleSubmit = () => {
+    var oldRes = res;
+    if (option) {
+      // oldRes[curr] = option;
+      oldRes[curr] = option == idcorrect;
+      setRes({ ...oldRes });
+      setCurr(curr + 1);
+      setIsChecked(false);
+    }
+  };
 
-  function chengeCheckbox() {
-    setChecked(!checked);
-  }
+  const handleCheck = () => {
+    if (option == idcorrect) {
+      console.log("pravilno");
+    } else console.log("net");
+    setIsChecked(true);
+  };
+
+  const [option, setOption] = useState(null);
+
+  const handleRadio = (val) => {
+    setOption(val);
+  };
+
+  const [isChecked, setIsChecked] = useState(null);
 
   return (
     <div className="form_wrapper_radio">
-      <div className="question_number">{id}</div>
-      <label className="text_label" htmlFor={id}>
-        {radioQuestion}
-      </label>
-      <input
-        className="text_radio"
-        name={id}
-        type="checkbox"
-        checked={checked}
-        onChange={chengeCheckbox}
-      />
+      <h1>{curr}</h1>
+      <div className="questions_wrapper">
+        <div
+          style={{
+            backgroundColor:
+              isChecked && id1 == idcorrect
+                ? "#0F0"
+                : isChecked && id1 !== idcorrect && id1 == option
+                ? "#F00"
+                : "",
+          }}
+          className="question_wrapper"
+        >
+          <input
+            type="radio"
+            id={id1}
+            name="123"
+            value={option}
+            onChange={(e) => handleRadio(id1)}
+          />
+          <label for="contactChoice1">{q1}</label>
+        </div>
+        <div
+          style={{
+            backgroundColor:
+              isChecked && id2 == idcorrect
+                ? "#0F0"
+                : isChecked && id2 !== idcorrect && id2 == option
+                ? "#F00"
+                : "",
+          }}
+          className="question_wrapper"
+        >
+          <input
+            type="radio"
+            id={id2}
+            name="123"
+            value={option}
+            onChange={(e) => handleRadio(id2)}
+          />
+          <label for="contactChoice1">{q2}</label>
+        </div>
+        <div
+          style={{
+            backgroundColor:
+              isChecked && id3 == idcorrect
+                ? "#0F0"
+                : isChecked && id3 !== idcorrect && id3 == option
+                ? "#F00"
+                : "",
+          }}
+          className="question_wrapper"
+        >
+          <input
+            type="radio"
+            id={id3}
+            name="123"
+            value={option}
+            onChange={(e) => handleRadio(id3)}
+          />
+          <label for="contactChoice1">{q3}</label>
+        </div>
+      </div>
+
+      <button
+        className="start_btn"
+        onClick={() => (isChecked ? handleSubmit() : handleCheck())}
+      >
+        {isChecked ? "Продолжить" : "Проверить"}
+      </button>
     </div>
   );
 }
